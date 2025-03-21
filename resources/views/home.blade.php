f283c055d169e7e499aab1c2dba61a80ed0da5b1
snydi <d-snyatkov@mail.ru>
2025-03-21 12:07:52 +0300

    Created the project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="yandex-verification" content="f1dc63d6385660f1"/>
+    <title>concord-test</title>
+    @vite(['resources/css/app.css'])
+</head>
+<body>
+<header class="header">
+</header>
+
+<div class="centered-container text-center">
+    <div class="text-center">
+      Здесь будут графики
+    </div>
+</div>
+
+</body>
+</html>
